3eed0c1f097f76c20eb41cd63adedd5ad47ac089
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-05-24 10:19:32 -0500

    condicion en home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,33 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
 
+    const navigate = useNavigate()
+    
+    useEffect(() => {
+        const token = window.localStorage.getItem('token')
+
+        const redirectFunction = () => navigate('/')
+    
+        if(!token){
+            redirectFunction()
+        }
+        
+    }, [])
+
+    const logOut = () => {
+        window.localStorage.removeItem('token')
+        navigate('/')
+    }
 
     return (
-        <h1>
-            Inicio de sesion Completada
-        </h1>
+        <div>
+            <h1>
+                Inicio de sesion Completada
+            </h1>
+            <button onClick={() => {logOut()}}>Log Out</button>
+        </div>
     )
 }
 
